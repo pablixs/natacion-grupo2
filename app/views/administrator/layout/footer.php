48e970dff3ffdcfa6e0031ba7700850bd09b25e8
Pablo <pablicjs@gmail.com>
2026-05-31 14:05:26 -0300

    fix: layouts modificados

--- app/views/administrator/layout/footer.php
+++ app/views/administrator/layout/footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer class="text-center mt-5 py-3 border-top">
-    <p>&copy; <?= date('Y') ?> - Aplicaciones WEB - Profesor lic. Juan Pablo Cesarini</p>
+    <!-- <p>&copy; <?= date('Y') ?> - Aplicaciones WEB - Profesor lic. Juan Pablo Cesarini</p> -->
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
