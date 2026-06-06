490355179d482d24178987d6ea65beada1e56246
Pablo <pablicjs@gmail.com>
2026-06-06 11:04:45 -0300

    fix: fix de base de datos

--- database/swimming_pool_g2.sql
+++ database/swimming_pool_g2.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 06-06-2026 a las 00:36:40
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: Jun 06, 2026 at 04:02 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `swimming_pool_g2`
+-- Database: `swimming_pool_g2`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `activity_log`
+-- Table structure for table `activity_log`
 --
 
 CREATE TABLE `activity_log` (
@@ -35,7 +35,7 @@ CREATE TABLE `activity_log` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `activity_log`
+-- Dumping data for table `activity_log`
 --
 
 INSERT INTO `activity_log` (`id`, `type`, `subject`, `ts`) VALUES
@@ -48,12 +48,14 @@ INSERT INTO `activity_log` (`id`, `type`, `subject`, `ts`) VALUES
 (8, 'swimmer_registered', 'pablicjs@gmail.com', '2026-06-05 10:44:56'),
 (9, 'profile_completed', 'Pablo Gomez', '2026-06-05 10:46:37'),
 (10, 'coach_registered', 'jgismylife@gmail.com', '2026-06-05 12:06:01'),
-(11, 'profile_completed', 'Luis Vega', '2026-06-05 12:06:40');
+(11, 'profile_completed', 'Luis Vega', '2026-06-05 12:06:40'),
+(12, 'coach_registered', 'perezjose@mailw.co', '2026-06-06 09:11:29'),
+(13, 'profile_completed', 'Santiago Ruiz', '2026-06-06 09:12:31');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `bookings`
+-- Table structure for table `bookings`
 --
 
 CREATE TABLE `bookings` (
@@ -67,14 +69,13 @@ CREATE TABLE `bookings` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `lessons`
+-- Table structure for table `lessons`
 --
 
 CREATE TABLE `lessons` (
   `id` int(11) NOT NULL,
   `coach_id` int(11) DEFAULT NULL,
-  `level` varchar(50) DEFAULT NULL,
-  `specialities` varchar(150) NOT NULL,
+  `level` enum('Principiante','Intermedio','Avanzado','') DEFAULT NULL,
   `first_day_of_week` enum('Lunes','Martes','Miércoles','Jueves','Viernes','Sabado') NOT NULL,
   `second_day_of_week` enum('Lunes','Martes','Miércoles','Jueves','Viernes','Sabado') DEFAULT NULL,
   `start_time` time DEFAULT NULL,
@@ -86,18 +87,51 @@ CREATE TABLE `lessons` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Volcado de datos para la tabla `lessons`
+-- Dumping data for table `lessons`
 --
 
-INSERT INTO `lessons` (`id`, `coach_id`, `level`, `specialities`, `first_day_of_week`, `second_day_of_week`, `start_time`, `end_time`, `capacity`, `active`, `created_at`, `deactivated_at`) VALUES
-(5, 117, 'Intermedio', 'Pecho, Espalda', 'Lunes', 'Miércoles', '09:00:00', '12:00:00', 10, 1, '2026-06-05 15:09:19', NULL),
-(6, 117, 'Intermedio', 'Pecho, Espalda', 'Lunes', '', '09:00:00', '12:00:00', 10, 1, '2026-06-05 15:21:14', NULL),
-(7, 117, 'Intermedio', 'Pecho, Espalda', 'Lunes', '', '09:00:00', '12:00:00', 10, 1, '2026-06-05 15:21:20', NULL);
+INSERT INTO `lessons` (`id`, `coach_id`, `level`, `first_day_of_week`, `second_day_of_week`, `start_time`, `end_time`, `capacity`, `active`, `created_at`, `deactivated_at`) VALUES
+(9, 117, 'Principiante', 'Lunes', 'Miércoles', '09:00:00', '12:00:00', 10, 1, '2026-06-05 23:28:48', NULL),
+(10, 117, 'Intermedio', 'Martes', 'Viernes', '15:00:00', '18:00:00', 15, 1, '2026-06-06 01:12:24', NULL),
+(11, 117, 'Avanzado', 'Miércoles', 'Sabado', '20:00:00', '21:00:00', 20, 1, '2026-06-06 01:13:04', NULL),
+(12, 117, 'Principiante', 'Lunes', 'Viernes', '15:00:00', '17:00:00', 5, 1, '2026-06-06 01:13:40', NULL),
+(13, 117, 'Avanzado', 'Lunes', 'Viernes', '08:00:00', '10:00:00', 10, 0, '2026-06-06 02:05:49', NULL),
+(14, 117, 'Intermedio', 'Lunes', 'Jueves', '08:00:00', '09:00:00', 20, 0, '2026-06-06 12:38:27', NULL),
+(15, 118, 'Intermedio', 'Lunes', 'Jueves', '08:00:00', '09:00:00', 20, 0, '2026-06-06 13:10:34', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `password_resets`
+-- Table structure for table `lessons_specialties`
+--
+
+CREATE TABLE `lessons_specialties` (
+  `lesson_id` int(11) NOT NULL,
+  `specialty_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `lessons_specialties`
+--
+
+INSERT INTO `lessons_specialties` (`lesson_id`, `specialty_id`) VALUES
+(9, 1),
+(9, 2),
+(10, 3),
+(10, 4),
+(11, 1),
+(12, 1),
+(12, 4),
+(13, 1),
+(13, 2),
+(14, 2),
+(15, 3),
+(15, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
 --
 
 CREATE TABLE `password_resets` (
@@ -110,18 +144,20 @@ CREATE TABLE `password_resets` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Volcado de datos para la tabla `password_resets`
+-- Dumping data for table `password_resets`
 --
 
 INSERT INTO `password_resets` (`id`, `user_id`, `email`, `token`, `expires_at`, `created_at`) VALUES
 (77, 115, 'pruebas@test.com', '36abe7791112bde9a46d6bac888d0fc5', '2026-06-01 21:57:26', '2026-06-02 00:56:46'),
 (78, 116, 'pablicjs@gmail.com', '21deb61d39539118bb4889fba4275224', '2026-06-05 10:46:37', '2026-06-05 13:44:56'),
-(79, 117, 'jgismylife@gmail.com', '08560094183c929b392ace54a1e3002f', '2026-06-05 12:06:40', '2026-06-05 15:06:01');
+(79, 117, 'jgismylife@gmail.com', '08560094183c929b392ace54a1e3002f', '2026-06-05 12:06:40', '2026-06-05 15:06:01'),
+(80, 118, 'perezjose@mailw.co', '06cc493304634d88e5a391e4084848cb', '2026-06-06 09:12:31', '2026-06-06 12:11:29'),
+(81, 116, 'pablicjs@gmail.com', '9534d673623390a39434650e1f14808bb423b63c8c5a5df0511fe77aaca39415', '2026-06-06 10:21:57', '2026-06-06 13:19:55');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `profiles`
+-- Table structure for table `profiles`
 --
 
 CREATE TABLE `profiles` (
@@ -139,18 +175,19 @@ CREATE TABLE `profiles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Volcado de datos para la tabla `profiles`
+-- Dumping data for table `profiles`
 --
 
 INSERT INTO `profiles` (`id`, `user_id`, `first_name`, `last_name`, `phone`, `specialty`, `birth_date`, `profile_image`, `created_at`, `updated_at`, `deleted_at`) VALUES
 (45, 115, 'Pruebas', 'Test', '1199228844', NULL, '2004-09-17', 'swimmer_ptest_4334.png', '2026-06-02 00:57:26', '2026-06-02 00:57:26', NULL),
 (46, 116, 'Pablo', 'Gomez', '1199887766', NULL, '2003-08-17', 'swimmer_pgomez_3884.jpg', '2026-06-05 13:46:37', '2026-06-05 13:46:37', NULL),
-(47, 117, 'Luis', 'Vega', '1100229933', 'mariposa', '2003-08-17', 'swimmer_lvega_6150.png', '2026-06-05 15:06:40', '2026-06-05 15:06:40', NULL);
+(47, 117, 'Luis', 'Vega', '1100229933', 'mariposa', '2003-08-17', 'swimmer_lvega_6150.png', '2026-06-05 15:06:40', '2026-06-05 15:06:40', NULL),
+(48, 118, 'Santiago', 'Ruiz', '1199228844', 'mariposa', '1994-05-18', 'default-profile.png', '2026-06-06 12:12:31', '2026-06-06 12:12:31', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `roles`
+-- Table structure for table `roles`
 --
 
 CREATE TABLE `roles` (
@@ -159,7 +196,7 @@ CREATE TABLE `roles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Volcado de datos para la tabla `roles`
+-- Dumping data for table `roles`
 --
 
 INSERT INTO `roles` (`id`, `role_name`) VALUES
@@ -170,7 +207,28 @@ INSERT INTO `roles` (`id`, `role_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `users`
+-- Table structure for table `specialties`
+--
+
+CREATE TABLE `specialties` (
+  `id` int(11) NOT NULL,
+  `specialty` enum('Crol','Espalda','Pecho','Mariposa') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `specialties`
+--
+
+INSERT INTO `specialties` (`id`, `specialty`) VALUES
+(1, 'Crol'),
+(2, 'Espalda'),
+(3, 'Pecho'),
+(4, 'Mariposa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -185,27 +243,28 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Volcado de datos para la tabla `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `role_id`, `profile_created`, `created_at`, `updated_at`, `deleted_at`) VALUES
 (115, 'pruebas@test.com', '$2y$10$O8sDRpBvLbyzoxggO718wemg8moxHh.0kgt/uhXETLc0Z8gLQmYTy', 1, 1, '2026-06-02 00:56:46', '2026-06-02 00:57:49', NULL),
-(116, 'pablicjs@gmail.com', '$2y$10$vmIcPQ.eJbOMLjvi.b.Z..kxxqU4vByORlYakTZ0fGJJaoa93d2M6', 3, 1, '2026-06-05 13:44:56', '2026-06-05 13:46:37', NULL),
-(117, 'jgismylife@gmail.com', '$2y$10$o3qhAkkfe8zSPd9YWPhYIOIK16.jQcvXkvL6K04eJOiEILYE/enwu', 2, 1, '2026-06-05 15:06:01', '2026-06-05 15:06:40', NULL);
+(116, 'pablicjs@gmail.com', '$2y$10$wPG09gF9ZMHPWJvcuvbTPOJDXm8c7OzKxXbkJC50ywNgoUbN8/eJS', 1, 1, '2026-06-05 13:44:56', '2026-06-06 13:21:57', NULL),
+(117, 'jgismylife@gmail.com', '$2y$10$o3qhAkkfe8zSPd9YWPhYIOIK16.jQcvXkvL6K04eJOiEILYE/enwu', 2, 1, '2026-06-05 15:06:01', '2026-06-05 15:06:40', NULL),
+(118, 'perezjose@mailw.co', '$2y$10$4w8HLuZ2oc7ZyrrrHIPod.O0rdCEO/7NUtqgc.JqX3Np2eCpyhJqO', 2, 1, '2026-06-06 12:11:29', '2026-06-06 12:12:31', NULL);
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `activity_log`
+-- Indexes for table `activity_log`
 --
 ALTER TABLE `activity_log`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_type` (`type`);
 
 --
--- Indices de la tabla `bookings`
+-- Indexes for table `bookings`
 --
 ALTER TABLE `bookings`
   ADD PRIMARY KEY (`id`),
@@ -213,14 +272,21 @@ ALTER TABLE `bookings`
   ADD KEY `fk_booking_lesson` (`lesson_id`);
 
 --
--- Indices de la tabla `lessons`
+-- Indexes for table `lessons`
 --
 ALTER TABLE `lessons`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_lesson_coach` (`coach_id`);
 
 --
--- Indices de la tabla `password_resets`
+-- Indexes for table `lessons_specialties`
+--
+ALTER TABLE `lessons_specialties`
+  ADD PRIMARY KEY (`lesson_id`,`specialty_id`),
+  ADD KEY `fk_specialtyid` (`specialty_id`);
+
+--
+-- Indexes for table `password_resets`
 --
 ALTER TABLE `password_resets`
   ADD PRIMARY KEY (`id`),
@@ -229,20 +295,26 @@ ALTER TABLE `password_resets`
   ADD KEY `fk_passwordResets_userId` (`user_id`);
 
 --
--- Indices de la tabla `profiles`
+-- Indexes for table `profiles`
 --
 ALTER TABLE `profiles`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_profile_user` (`user_id`);
 
 --
--- Indices de la tabla `roles`
+-- Indexes for table `roles`
 --
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `users`
+-- Indexes for table `specialties`
+--
+ALTER TABLE `specialties`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
@@ -251,76 +323,89 @@ ALTER TABLE `users`
   ADD KEY `fk_user_role` (`role_id`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `activity_log`
+-- AUTO_INCREMENT for table `activity_log`
 --
 ALTER TABLE `activity_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT de la tabla `bookings`
+-- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `lessons`
+-- AUTO_INCREMENT for table `lessons`
 --
 ALTER TABLE `lessons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT de la tabla `password_resets`
+-- AUTO_INCREMENT for table `password_resets`
 --
 ALTER TABLE `password_resets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
 
 --
--- AUTO_INCREMENT de la tabla `profiles`
+-- AUTO_INCREMENT for table `profiles`
 --
 ALTER TABLE `profiles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
--- AUTO_INCREMENT de la tabla `roles`
+-- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de la tabla `users`
+-- AUTO_INCREMENT for table `specialties`
+--
+ALTER TABLE `specialties`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `lessons`
+-- Constraints for table `lessons`
 --
 ALTER TABLE `lessons`
   ADD CONSTRAINT `fk_coachId` FOREIGN KEY (`coach_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `password_resets`
+-- Constraints for table `lessons_specialties`
+--
+ALTER TABLE `lessons_specialties`
+  ADD CONSTRAINT `fk_lessonsid` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_specialtyid` FOREIGN KEY (`specialty_id`) REFERENCES `specialties` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `password_resets`
 --
 ALTER TABLE `password_resets`
   ADD CONSTRAINT `fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_mail` FOREIGN KEY (`email`) REFERENCES `users` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `profiles`
+-- Constraints for table `profiles`
 --
 ALTER TABLE `profiles`
   ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `users`
+-- Constraints for table `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `fk_roleid` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
